5dfbcd33bc52befdd8edcc23350a840632961202
robinvanpersie <1260896224@qq.com>
2018-04-21 16:00:09 +0800

    commit

--- RunsheungIOS/portal/Portal/Supermarket/SupermarketMine/SupermarketMyOrderController.m
+++ RunsheungIOS/portal/Portal/Supermarket/SupermarketMine/SupermarketMyOrderController.m
@@ -55,8 +55,7 @@
 			for (UIViewController *vc in controllers) {
 				if ([vc isMemberOfClass:[SupermarketHomeViewController class]]||[vc isMemberOfClass:[LZCartViewController class]]) {
 					[self.navigationController popToViewController:vc animated:YES];
-					
-				}
+                }
 			}
 
 		} else {
